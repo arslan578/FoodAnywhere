a3e39b4e734f526ccc664c15117640988813ac90
arslan578 <arslanzaheer977@gmail.com>
2025-02-04 12:21:52 +0000

    resolve setup errors

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,13 +5,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": config("POSTGRES_DBNAME"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST"),
-        "PORT": config("POSTGRES_PORT"),
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',  # or 'django.db.backends.postgresql'
+        'NAME': os.environ.get('POSTGRES_DB', 'foodanywhere_local'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
